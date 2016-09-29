90b112aabe9dbf33c024580d8be6be990d3e07be
Pattelicious <patrick.voss1808@gmail.com>
2016-09-29 19:58:05 +0200

    Fix bug where textinput did re-render itself everytime

--- src/components/PollAnswersForm.js
+++ src/components/PollAnswersForm.js
@@ -3,19 +3,11 @@ import TextField from 'material-ui/TextField';
 
 const PollAnswersForm = ({answers, addPollAnswer}) => {
 
-  const onAddPollAnswer = (key) => {
-    console.log(answers.length);
-    console.log(key);
-    if(answers.length - 1 === key) {
-      addPollAnswer();
-    }
-  };
-
   return (
     <div>
     {answers.map((obj, key) => (
       <div key={key}>
-        <TextField hintText="Enter poll option" onKeyUp={onAddPollAnswer(key)} /><br />
+        <TextField hintText="Enter poll option" onChange={() => (key === answers.length - 1) ? addPollAnswer() : null} /><br />
       </div>
     ))}
     </div>
